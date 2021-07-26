070bfaa08297eb8320e23b99e890591655aaf156
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-26 12:13:10 +0300

    test

---
+++ lesson_1/task_1/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Dimensions from "./Dimensions.jsx";
+
+
+const App = () => {
+  return <Dimensions />;
+};
+
+export default App;
