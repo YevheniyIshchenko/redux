fc84dcaa0cd23db06e31d6bd9bfdfa4ec9b9659d
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-26 12:40:55 +0300

    task1

--- lesson_1/task_1/src/App.jsx
+++ lesson_1/task_1/src/App.jsx
@@ -1,10 +1,31 @@
 import React from "react";
-// import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import Dimensions from "./Dimensions.jsx";
+import ThemedButton from "./ThemedButton.jsx";
+import { themes, ThemeContext } from "./themes-context.js";
 
+class App extends React.Component {
+  state = {
+    theme: themes.light,
+  };
 
-const App = () => {
-  return <Dimensions />;
-};
+  toggleTheme = () => {
+    const newTheme =
+      this.state.theme === themes.dark ? themes.light : themes.dark;
+
+    this.setState({
+      theme: newTheme,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <ThemedButton onClick={this.toggleTheme}>Dynamic Theme</ThemedButton>
+        </ThemeContext.Provider>
+        <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
+      </div>
+    );
+  }
+}
 
 export default App;
