911c1197450913a3851532559ae6af7600c1a45f
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-28 17:02:53 +0300

    task1

---
+++ lesson_4/task_1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux";
+import Counter from "./Counter";
+import store from "./store.js";
+const App = () => {
+  return(
+  <Provider store={store}>
+    <Counter />;
+  </Provider>)
+};
+
+export default App;
