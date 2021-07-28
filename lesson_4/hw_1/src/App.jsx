3f77b4d16ddb143e24ee27ff2b5d8a01dd6bb43c
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-28 20:27:38 +0300

    hw1

--- lesson_4/hw_1/src/App.jsx
+++ lesson_4/hw_1/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import UsersList from "./users/usersList";
+import UsersList from "./users/UsersList";
 import store from "./store.js";
 const App = () => {
   return (
