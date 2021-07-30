ab2754027fdbc0dfbe03be2ae63c73c439c97d20
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-30 13:50:30 +0300

    task1

---
+++ lesson_5/task_1/src/users/Pagination.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Pagination = ({
+  goPrev,
+  goNext,
+  currentPage,
+  totalItems,
+  itemsPerPage,
+}) => {
+  const isPrevPageAvailable = currentPage === 0;
+  const isNextPageAvailable = (currentPage + 1) * itemsPerPage > totalItems;
+  return (
+    <div className='pagination'>
+      <button className='btn' onClick={goPrev} disabled={isPrevPageAvailable}>
+        {!isPrevPageAvailable ? "←" : null}
+      </button>
+      <span className='pagination__page'>{currentPage + 1}</span>
+      <button className='btn' onClick={goNext} disabled={isNextPageAvailable}>
+        {!isNextPageAvailable ? "→" : null}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
